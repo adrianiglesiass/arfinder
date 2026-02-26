edb77406d867213384f78e10d8140863c2e0734e
Adrian Iglesias <adrianiglesias010@gmail.com>
2026-02-26 13:31:52 +0100

    feat: add user pydantic schemas

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[int] = None
