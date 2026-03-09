bbb318e34e38b65cfd4301914904b7f4bfeb5428
Adrian Iglesias <adrianiglesias010@gmail.com>
2026-03-09 20:02:30 +0100

    feat: add profile_photo model and migration

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.user import User as User
 from app.models.profile import Profile as Profile
 from app.models.conversation import Conversation as Conversation
 from app.models.message import Message as Message
+from app.models.profile_photo import ProfilePhoto as ProfilePhoto
