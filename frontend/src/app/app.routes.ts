d043b4b5d77f242161f5d841e5a4bf8bd53c5388
adrianiglesiass <adrianiglesias010@gmail.com>
2026-03-18 13:14:30 +0100

    feat: add jwt interceptor and authentication guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@core/auth/auth-guard';
 import { Layout } from '@core/layout/layout';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    // loadComponent: () => import('@features/auth/login'),
+  },
+
+  {
+    path: 'register',
+    // loadComponent: () => import('@features/auth/register'), cuando cree los componentes
+  },
+
+  {
     path: '',
     component: Layout,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
@@ -16,6 +28,6 @@ export const routes: Routes = [
 
   {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'login',
   },
 ];
