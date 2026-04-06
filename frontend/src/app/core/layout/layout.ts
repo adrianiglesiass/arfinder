9c280ff3e318780034c1a02da71e8eb0249dac69
Adrián Iglesias Álvarez <adrianiglesias010@gmail.com>
2026-04-06 15:43:34 +0200

    feat: implement auth card component and refactor login/register forms to use it (#83)

--- frontend/src/app/core/layout/layout.ts
+++ frontend/src/app/core/layout/layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArfinderLogo } from '@shared/components/arfinder-logo/arfinder-logo';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArfinderLogo],
   templateUrl: './layout.html',
 })
 export class Layout {}
---
+++ frontend/src/app/shared/components/auth-card/auth-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ArfinderLogo } from '@shared/components/arfinder-logo/arfinder-logo';
+
+@Component({
+  selector: 'app-auth-card',
+  imports: [ArfinderLogo],
+  templateUrl: './auth-card.html',
+})
+export class AuthCard {}
