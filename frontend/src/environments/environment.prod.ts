b7be9139c70ca27df6e3be0718ea8e4a65efb3f1
adrianiglesiass <adrianiglesias010@gmail.com>
2026-05-07 20:54:53 +0200

    chore(deploy): add fly.io backend config, insforge frontend deploy workflows and environment artifacts

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  APIURL: 'https://arfinder-api.fly.dev',
+  insforge: {
+    url: 'https://bk4jh2ga.eu-central.insforge.app',
+    apiKey: 'ik_23c981706020779f17181d799ee61973',
+    redirectUri: 'https://arfinder.insforge.site/auth/callback',
+  },
+};
